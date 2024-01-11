e238f37ef57217e6ed06be526787439c7c25f787
Shatrov-228 <84198387+Shatrov-228@users.noreply.github.com>
2024-01-11 21:03:28 +0300

    tr_tv.py

--- tr_tv.py
+++ tr_tv.py
@@ -1,13 +1,17 @@
-s1 = 'GCAACGCACAACGAAAACCCTTAGGGACTGGATTATTTCGTGATCGTTGTAGTTATTGGAAGTACGGGCATCAACCCAGTT'
-s2 = 'TTATCTGACAAAGAAAGCCGTCAACGGCTGGATAATTTCGCGATCGTGCTGGTTACTGGCGGTACGAGTGTTCCTTTGGGT'
-dna_dict = {'AG': 'transition', 'CT': 'transition', 'AC': 'transversion', 'GT': 'transversion', 'AT': 'transversion', 'CG': 'transversion'}
+def transition_transversion_ratio(s1, s2):
+    transition_transversion_dict = {'AG': 'transition', 'CT': 'transition', 'AC': 'transversion', 'GT': 'transversion', 'AT': 'transversion', 
+                                    'CG': 'transversion'}
     transition = 0
-    transvertion = 0
+    transversion = 0
     for nucl in range(0, len(s1)):
         if s1[nucl] != s2[nucl]:
-            identifier = ''.join(sorted([s1[nucl], s2[nucl]]))
-            if dna_dict[identifier] == 'transition':
+            current_key = ''.join(sorted([s1[nucl], s2[nucl]]))
+            if transition_transversion_dict[current_key] == 'transition':
                 transition += 1
             else:
-                transvertion += 1
-print(f'Кол-во транзиций = {transition}, кол-во трансверсий = {transversion}, коэффициент = {transition / transvertion}')
+                transversion += 1
+    print(f'Кол-во транзиций = {transition}, кол-во трансверсий = {transversion}, коэффициент = {transition / transversion}')
+
+s1 = 'GCAACGCACAACGAAAACCCTTAGGGACTGGATTATTTCGTGATCGTTGTAGTTATTGGAAGTACGGGCATCAACCCAGTT'
+s2 = 'TTATCTGACAAAGAAAGCCGTCAACGGCTGGATAATTTCGCGATCGTGCTGGTTACTGGCGGTACGAGTGTTCCTTTGGGT'
+transition_transversion_ratio(s1, s2)
